fdbe3feb4c7ea18255fb0a12048e6d002957ccd0
Ranjeetkuamr <ranjeetk5498@gmail.com>
2024-03-23 02:38:05 +0530

    prediction pipeline

---
+++ src/pipeline/prediction_pipeline.py
@@ -0,0 +1,65 @@
+import sys
+import pandas as pd
+import os
+from src.exception import CustomException
+from src.utils import load_object
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','proprocessor.pkl')
+            print("Before Loading")
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+
+class CustomData:
+    def __init__(self,
+                 carat:float,
+                 depth:float,
+                 table:float,
+                 x:float,
+                 y:float,
+                 z:float,
+                 cut:str,
+                 color:str,
+                 clarity:str):
+        
+        self.carat=carat
+        self.depth=depth
+        self.table=table
+        self.x=x
+        self.y=y
+        self.z=z
+        self.cut = cut
+        self.color = color
+        self.clarity = clarity
+    def get_data_as_dataframe(self):
+        try:
+            custom_data_input_dict = {
+                'carat':[self.carat],
+                'depth':[self.depth],
+                'table':[self.table],
+                'x':[self.x],
+                'y':[self.y],
+                'z':[self.z],
+                'cut':[self.cut],
+                'color':[self.color],
+                'clarity':[self.clarity]
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+        
+        except Exception as e:
+            raise CustomException(e, sys)

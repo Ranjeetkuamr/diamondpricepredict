d1fb38db72fd9932608fefbde26d8828c033e5b6
Ranjeetkuamr <ranjeetk5498@gmail.com>
2024-03-23 11:25:02 +0530

    Diamond predict

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def predict_datapoint():
         
         results=predict_pipeline.predict(pred_df)
         
-        return render_template('home.html',results=results[0])
+        return render_template('home.html',final_result=results[0])
     
 
 if __name__=="__main__":
